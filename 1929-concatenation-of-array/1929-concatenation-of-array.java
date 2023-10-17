8fa36efad75cc19d6f3436b05c99e2bd26816870
S-a-m-93 <124641896+S-a-m-93@users.noreply.github.com>
2023-10-17 18:30:07 +0530

    Time: 1 ms (96.91%), Space: 44.2 MB (71.29%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[2*n];
+  
+            for(int j =0; j < n; j ++){
+                ans[j] = nums[j];
+                ans[n + j] = nums[j];
+            }
+            
+     
+        return ans;
+        
+    }
+}
